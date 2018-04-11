5b921d95e692c79b139cadf82982946519b54743
Hubert Springer <[springi775@gmail.com]>
2018-04-11 14:37:59 +0200

    hubert

--- pytong.py
+++ pytong.py
@@ -2,7 +2,6 @@ import copy
 import json
 from PIL import Image, ImageTk
 import tkinter as tk
-import argparse
 import cv2
 
 
@@ -23,7 +22,6 @@ class old_play(): #klasa przechowującą zapisaną rozgrywkę
         else:
             return self.json_game_history[round_number]["pawns"]
 
-
 gra = old_play('trial_game.txt')
 
 
@@ -42,8 +40,11 @@ class Checkers_Board(): # klasa zajmująca się wizualizacją
         self.board = copy.deepcopy(self.sprite)
         for x in range(len(self.Matrix)):
             for y in range(len(self.Matrix[x])):
-                if self.Matrix[y][x]!= PositionMatrix[y][x]:
+                if self.Matrix[y][x] != PositionMatrix[y][x]:
+                    if PositionMatrix[y][x] > 0 :
                         cv2.rectangle(self.board, (2+(x * 100), 2+(y * 100)), (98+(x * 100), 98+(y * 100)), (0, 255, 0), 4)
+                    else:
+                        cv2.rectangle(self.board, (2 + (x * 100), 2 + (y * 100)), (98 + (x * 100), 98 + (y * 100)),(0, 0, 255), 4)
                 if PositionMatrix[y][x] == 0:
                     continue
                 elif PositionMatrix[y][x] == 1:
@@ -69,18 +70,23 @@ class Checkers_Board(): # klasa zajmująca się wizualizacją
 
 class Application:
     def __init__(self, output_path = "./"):
-        self.vs = cv2.VideoCapture(0) # klatki z kamerki, 0 to domyślna
+        self.vs = cv2.VideoCapture(1) # klatki z kamerki, 0 to domyślna
         self.output_path = output_path  # sciezka wyjsciowa
         self.current_image = None  # aktualny obraz z kamery
         self.actual_round = 0;
 
         self.root = tk.Tk()  # inicjalizacja rooota
         self.root.title("Checkers")  # tytul okna
-        # self.destructor function gets fired when the window is closed
         self.root.protocol('WM_DELETE_WINDOW', self.destructor) # destrucor odpala się po zamknięciu okna
 
-        self.panel = tk.Label(self.root)  # inicjalizacja panelu z kamera
-        self.panel.grid(row=0, column=0)
+        frameleft = tk.Frame(self.root)
+        frameleft.grid(row=0, column=0)
+
+        self.panel = tk.Label(frameleft)  # inicjalizacja panelu z kamera
+        self.panel.pack(side="top", fill="both", expand=1)
+
+        self.panel2 = tk.Label(frameleft)
+        self.panel2.pack(side="bottom", fill="both", expand=1)
 
 
         self.Checkers_panel = tk.Label(self.root)
@@ -97,27 +103,56 @@ class Application:
         btn = tk.Button(f1, text="move forward", command=self.move_forward)  # przycisk do kolejnej tury
         btn.pack(side="left", fill="both", expand=1, padx=20, pady=20)
 
+        f2 = tk.Frame(self.root)
+        f2.grid(row=1, column=0, sticky="nsew")
 
+        Frame_Slider = tk.Frame(self.root)
+        f1.grid(row=0, column=2)
 
+        scrollbar = tk.Scrollbar(Frame_Slider)
+        scrollbar.pack(side="right", fill="y")
 
+        mylist = tk.Listbox(Frame_Slider, yscrollcommand=scrollbar.set)
+        for round_number in gra.json_game_history:
+            mylist.insert(tk.END, str(round_number) + "This is round number ")
 
+        mylist.pack(side="left", fill="both")
+        scrollbar.config(command=mylist.yview)
 
         self.video_loop()
+
+    def Slider_function(self,val):
+        print(val)
+
 
     def video_loop(self):
         """ Get frame from the video stream and show it in Tkinter """
         ok, frame = self.vs.read()  # read frame from video stream
         if ok:  # frame captured without any errors
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
+
             self.current_image = Image.fromarray(cv2image)  # convert image for PIL
             imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter
             self.panel.imgtk = imgtk  # anchor imgtk so it does not be deleted by garbage-collector
             self.panel.config(image=imgtk)  # show the image
+
+
+            imgtk2 = cv2.resize(cv2.Canny(cv2image, 100, 200),(400,400))
+
+
+            wow = Image.fromarray(imgtk2)
+            imgtk2 = ImageTk.PhotoImage(image=wow)
+            self.panel2.imgtk = imgtk2  # anchor imgtk so it does not be deleted by garbage-collector
+            self.panel2.config(image=imgtk2)  # show the image
+
         self.root.after(30, self.video_loop)  # call the same function after 30 milliseconds
 
     def move_forward(self):
-        self.actual_round +=1
-        self.board_game.draw(gra.return_round(self.actual_round))
+        if self.actual_round < len(gra.json_game_history) - 1:
+            self.actual_round +=1
+            self.board_game.draw(gra.return_round(self.actual_round))
+        else:
+            print("Ostatni ruch")
 
     def move_backward(self):
         if self.actual_round > 0 :
