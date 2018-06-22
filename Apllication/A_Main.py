1fff1b102396b76dabbdd27b00e602d47d229ca6
Szamaragdowy <springi775@gmail.com>
2018-06-22 20:36:31 +0200

    ip

--- Apllication/A_Main.py
+++ Apllication/A_Main.py
@@ -13,6 +13,7 @@ class Application:
         self.load_background()
         self.create_buttons()
         self.root.protocol('WM_DELETE_WINDOW', self.destructor)
+        self.center()
 
     def create_window(self):
         self.root = tk.Tk()  # inicjalizacja rooota
@@ -49,3 +50,11 @@ class Application:
 
     def destructor(self):
         self.root.destroy()
+
+    def center(self):
+        self.root.update_idletasks()
+        width =  self.root.winfo_width()
+        height =  self.root.winfo_height()
+        x = ( self.root.winfo_screenwidth() // 2) - (width // 2)
+        y = ( self.root.winfo_screenheight() // 2) - (height // 2)
+        self.root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
--- Apllication/A_OptionsView.py
+++ Apllication/A_OptionsView.py
@@ -7,18 +7,15 @@ from Backend.BoardDetection import *
 
 class OptionsClass:
     def __init__(self):
-        self.vs = cv2.VideoCapture(0)
-        self.current_image = None  # aktualny obraz z kamery
         self.CreateOptionWindow()
 
     def CreateOptionWindow(self):
         self.OptionsWindow = tk.Toplevel()  # inicjalizacja rooota
         self.OptionsWindow.title("Options")  # tytul okna
         self.OptionsWindow.protocol('WM_DELETE_WINDOW', self.destructor) # destrucor odpala się po zamknięciu okna
-        self.CameraIPTextBox = tk.StringVar(self.OptionsWindow, value='http://192.168.137.70:4747/video%27')
+        self.CameraIP = tk.StringVar(self.OptionsWindow, value='http://192.168.137.70:4747/video%27')
         self.InitializePanels()
-        self.InitializeCameraPanel()
-        self.video_loop()
+        self.center()
 
     def InitializeRightPanel(self):
         self.RightPanel = tk.Label(self.OptionsWindow)
@@ -26,40 +23,8 @@ class OptionsClass:
         self.InitializeRightPanelComponent()
 
     def InitializeRightPanelComponent(self):
-        self.InitializeIpButton()
-        self.InitializeIPEntry()
+        tk.Entry(self.RightPanel, width=50, textvariable=self.CameraIP).grid(row=0, column=2) #ip entry
         self.InitializePlayButton()
-        self.InitializeSliders()
-
-    def InitializeSliders(self):
-        frame_Slider = tk.Frame(self.RightPanel)
-        frame_Slider.grid(row=1, column=2)
-
-        tk.Label(self.RightPanel,text = 'Gracz 1' ).grid(row=1, column=1)
-        tk.Label(frame_Slider, text='MIN').grid(row=0, column=0)
-        tk.Label(frame_Slider, text='MAX').grid(row=0, column=2)
-
-        tk.Scale(frame_Slider, from_=0, to=180, orient='horizontal').grid(row=0, column=1)
-        tk.Scale(frame_Slider, from_=0, to=180, orient='horizontal').grid(row=0, column=3)
-
-
-
-        frame_Slider2 = tk.Frame(self.RightPanel)
-        frame_Slider2.grid(row=2, column=2)
-
-        tk.Label(self.RightPanel,text = 'Gracz 2' ).grid(row=2, column=1)
-        tk.Label(frame_Slider2, text='MIN').grid(row=0, column=0)
-        tk.Label(frame_Slider2, text='MAX').grid(row=0, column=2)
-
-        tk.Scale(frame_Slider2, from_=0, to=180, orient='horizontal').grid(row=0, column=1)
-        tk.Scale(frame_Slider2, from_=0, to=180, orient='horizontal').grid(row=0, column=3)
-
-    def InitializeIpButton(self):
-        button1 = tk.Button(self.RightPanel, text='USTAW IP', command=self.InitializeCamera)
-        button1.grid(row=0, column=1)  # pozycjonowanie
-
-    def InitializeIPEntry(self):
-        nentry = tk.Entry(self.RightPanel, width=50, textvariable=self.CameraIPTextBox).grid(row=0, column=2)
 
     def InitializePlayButton(self):
         button1 = tk.Button(self.RightPanel, text='Przejdź do GRY', command=self.GoToCaptureCheckers)
@@ -67,36 +32,18 @@ class OptionsClass:
 
     def InitializePanels(self):
         self.InitializeRightPanel()
-        self.InitializeCameraPanel()
-
-    def InitializeCameraPanel(self):
-        self.Camerapanel = tk.Label(self.OptionsWindow)  # inicjalizacja panelu z kamera
-        self.Camerapanel.grid(row=0, column=0)  # pozycjonowanie panelu
 
     def GoToCaptureCheckers(self):
-        #if self.vs.isOpened():
-         #   CaptureCheckersWindow(vs=self.vs)
-        #else:
-         #   CaptureCheckersWindow(ip=self.CameraIPTextBox.get())
-
-        CaptureCheckersWindow(ip ='http://192.168.1.31:4747/video%27')
-        #self.OptionsWindow.destroy()
-
-    def InitializeCamera(self):
-        self.vs = cv2.VideoCapture(self.CameraIPTextBox.get()) # klatki z kamerki, 0 to domyślna
-
-    def video_loop(self):
-        ok, frame = self.vs.read()  # read frame from video stream
-        if ok:  # frame captured without any errors
-            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
-
-            self.current_image = Image.fromarray(cv2image)  # convert image for PIL
-            imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter
-            self.Camerapanel.imgtk = imgtk  # anchor imgtk so it does not be deleted by garbage-collector
-            self.Camerapanel.config(image=imgtk)  # show the image
-
-        self.OptionsWindow.after(30, self.video_loop)  # call the same function after 30 milliseconds
+        CaptureCheckersWindow(ip=self.CameraIP.get())
+        self.OptionsWindow.destroy()
 
     def destructor(self):
         self.OptionsWindow.destroy()
-        self.vs.release()
+
+    def center(self):
+        self.OptionsWindow.update_idletasks()
+        width =  self.OptionsWindow.winfo_width()
+        height =  self.OptionsWindow.winfo_height()
+        x = ( self.OptionsWindow.winfo_screenwidth() // 2) - (width // 2)
+        y = ( self.OptionsWindow.winfo_screenheight() // 2) - (height // 2)
+        self.OptionsWindow.geometry('{}x{}+{}+{}'.format(width, height, x, y))
